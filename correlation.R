620ed8970c5613fad0b2cb597861c97a4af79ba2
avery <asommer999@gmail.com>
2020-12-14 16:49:09 -0500

    added corrplots EDA

---
+++ correlation.R
@@ -0,0 +1,20 @@
+library(ggcorrplot)
+library(ggplot2)
+library(dplyr)
+
+# columns we want to look at to analyze
+cols <- c("location", "total_cases_per_million", "total_deaths_per_million", "total_tests_per_thousand", "gdp_per_capita", "hospital_beds_per_thousand", "human_development_index")
+
+# create data
+data.covid <- dec9covid[,cols]
+# merge covid data with health data
+data.covidHealth <- merge(data.covid, exp2017_health[exp2017_health$Series == "Current health expenditure (% of GDP)",c("Country","Value")], by.x = "location", by.y="Country")
+data.covidHealth <- data.covidHealth %>% rename(curr_health_exp = Value)
+data.covidHealth <- merge(data.covid, exp2017_health[exp2017_health$Series == "Domestic general government health expenditure (% of total government expenditure)",c("Country","Value")], by.x = "location", by.y="Country")
+data.covidHealth <- data.covidHealth %>% rename(dom_gov_health_exp = Value)
+
+# create corrplot
+plot <- ggcorrplot(cor(data.covidHealth[,-1], use="complete.obs"),type = "lower",
+                   lab = TRUE,
+                   ggtheme = ggplot2::theme_minimal(), tl.col="white")
+ggsave("./plots/corrplot/plot.png", width = 7, height=7)
